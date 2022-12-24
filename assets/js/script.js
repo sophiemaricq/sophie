2c933d3a80a9835879116b6460d8e67a543aab60
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-12-24 15:43:16 +0000

    Set up audio track on first button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
-$(document).ready(function() {
+
     var playing = false;
+
+    let audio_track1 = document.getElementById("audio_track1");
+    let play_button1 = document.getElementById("play1");
+
+
+    play_button1.addEventListener('click', playMusic);
+
+    function playMusic() {
+        console.log("play clicked");
+        if (playing == false) {
+            audio_track1.play()
+            playing = true;
+            console.log("playing")
+            this.find('i').classList.add("far fa-pause-circle")
+            this.find('i').classList.remove("far fa-play-circle")
+        } else {
+            audio_track1.pause()
+            playing = false;
+            console.log("paused")
+        }
+    }
+
     
 
-
-    $('.audio-btn').click(function() {
-        if (playing == false) {
-            document.getElementById('myAudio').play();
-            playing = true;
-            $(this).text("stop sound");
-
-        } else {
-            document.getElementById('myAudio').pause();
-            playing = false;
-            $(this).text("restart sound");
-        }
-    });
-
-});
